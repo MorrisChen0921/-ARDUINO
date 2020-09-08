ccd57c8d4849ea0606bfd5ee9ab3a1bebe4f5d0c
MorrisChen0921 <54127872+MorrisChen0921@users.noreply.github.com>
2020-09-08 14:26:54 +0800

    Add files via upload
    4顆LED同時亮滅

---
+++ sketch_sep08a-1.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+for(int i=2;i<6;i++)
+   pinMode(i,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(int i=2;i<6;i++) 
+ digitalWrite(i,HIGH);//滅
+ delay(100);
+ for(int i=2;i<6;i++)
+   digitalWrite(i,LOW);//亮
+ delay(100);
+｝
